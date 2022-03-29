54dc2e40245f96d3c22298c90260018e5d12a04e
sogaiu <983021772@users.noreply.github.com>
2022-03-29 15:20:48 +0900

    Initial commit

---
+++ queries/highlights.scm
@@ -0,0 +1,25 @@
+(num_lit) @number
+
+[
+ (char_lit)
+ (str_lit)
+] @string
+
+[
+ (bool_lit)
+ (nil_lit)
+] @constant.builtin
+
+(kwd_lit) @constant
+
+(comment) @comment
+
+;; Treat quasiquotation as operators for the purpose of highlighting.
+
+[
+ "'"
+ "`"
+ "~"
+ "@"
+ "~@"
+] @operator
